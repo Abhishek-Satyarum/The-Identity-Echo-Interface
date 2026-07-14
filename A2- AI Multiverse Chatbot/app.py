eef417abd6d08d96b804014e1d05c6f7d00dd7ac
Abhishek-Satyarum <satyarumabhishek@gmail.com>
2026-07-14 19:12:13 +0530

    Upgraded AI Multiverse

--- A2- AI Multiverse Chatbot/app.py
+++ A2- AI Multiverse Chatbot/app.py
@@ -7,11 +7,44 @@ load_dotenv()
 st.title("Multiverse Chatbot")
 personality = st.sidebar.selectbox("Select anyone", ["A.P.J Abdul kalam", "An expert Hacker", "Donald Trump", "Sherlock Holmes", "Albert Einstein", "Elon Musk", "Stand-up Comedian", "Friendly Teacher", "Motivational Coach", "Angry HOD"])
 
-intensity = st.sidebar.slider("Intensity", min_value=0, max_value=10)
+intensity = st.sidebar.slider("Intensity", min_value=1, max_value=10)
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 user_message = st.text_input("Say something : ")
 if st.button("SEND"):
     if user_message:
+        if personality == "A.P.J Abdul kalam":
+            bot_avatar = "🚀"
+
+        elif personality == "An expert Hacker":
+            bot_avatar = "💻"
+
+        elif personality == "Donald Trump":
+            bot_avatar = "🇺🇸"
+
+        elif personality == "Sherlock Holmes":
+            bot_avatar = "🕵️"
+
+        elif personality == "Albert Einstein":
+            bot_avatar = "🧠"
+
+        elif personality == "Elon Musk":
+            bot_avatar = "🚀"
+
+        elif personality == "Stand-up Comedian":
+            bot_avatar = "😂"
+
+        elif personality == "Friendly Teacher":
+            bot_avatar = "👨‍🏫"
+
+        elif personality == "Motivational Coach":
+            bot_avatar = "💪"
+
+        elif personality == "Angry HOD":
+            bot_avatar = "😠"
+
+        else:
+            bot_avatar = "🤖"
+            
         ai_instructions= f"You are acting as {personality}.Make sure to maintain the intensity of reply based on {intensity}. Respond to the message sent by the user staying completely in character: {user_message} "
 
         with st.spinner("Connecting Multiverse...."):
@@ -19,8 +52,11 @@ if st.button("SEND"):
                 model= "gemini-2.5-flash",
                 contents=ai_instructions
             )
-            st.success("Message received!")
-            st.markdown(response.text)
+            with st.chat_message("user"):
+                st.write(user_message)
+            
+            with st.chat_message("assistant", avatar= bot_avatar):
+                st.write(response.text)
 
     else :
         st.warning("Please type a message first")
